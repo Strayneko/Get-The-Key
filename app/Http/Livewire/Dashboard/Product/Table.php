d8477fba9551409a36d8ac197d1ba6961edc4887
strayneko <bayurendi10@gmail.com>
2023-01-02 09:13:21 +0700

    :zap: add improvements to product delete feature

--- app/Http/Livewire/Dashboard/Product/Table.php
+++ app/Http/Livewire/Dashboard/Product/Table.php
@@ -7,16 +7,19 @@ use App\Models\Product;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use App\Models\License;
 
 class Table extends Component
 {
     public $products;
     protected $listeners = ['refreshTable' => '$refresh'];
+    public $shop;
     public function deleteProduct($id)
     {
         $product = Product::find($id);
         if (!$product) abort(404);
         Storage::disk('public')->delete($product->image);
+        if ($product->license) License::where('product_id', $id)->delete();
         $product->delete();
         session()->flash('success', 'Data has been deleted');
         $this->emit('refreshTable');
